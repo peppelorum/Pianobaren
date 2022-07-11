66e2c5619f66397b26c18a50f1f450c77874eaa3
Peppe Hämeenniemi <p@bergqvi.st>
2022-07-11 19:21:37 +0200

    setup script

--- setup.sh
+++ setup.sh
@@ -3,8 +3,17 @@
 python3 -m venv .venv &
 source .venv/bin/activate &
 pip3 install -r requirements.txt &
+
+sudo systemctl stop server.service &
+sudo cp services/server.service /etc/systemd/system/server.service &
+sudo systemctl daemon-reload &
+sudo systemctl start server.service &
+sudo systemctl status server.service
+
 sudo systemctl stop buttons.service &
 sudo cp services/buttons.service /etc/systemd/system/buttons.service &
 sudo systemctl daemon-reload &
 sudo systemctl start buttons.service &
 sudo systemctl status buttons.service
+
+
