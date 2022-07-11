cee692fc0a958b7eeda96a2c25f6a9f8af548eab
Peppe Hämeenniemi <p@bergqvi.st>
2022-07-11 19:16:17 +0200

    setup script

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 python3 -m venv .venv &
 source .venv/bin/activate &
 pip3 install -r requirements.txt &
-systemctl stop bar.service &
-cp bar.service /etc/systemd/system/bar.service &
-systemctl daemon-reload &
-systemctl start bar.service &
+sudo systemctl stop bar.service &
+sudo cp bar.service /etc/systemd/system/bar.service &
+sudo systemctl daemon-reload &
+sudo systemctl start bar.service &
 sudo systemctl status bar.service
