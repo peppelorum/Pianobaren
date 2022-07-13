890cabcbd0054f73ea347ada79d3005f890083b9
Peppe Hämeenniemi <p@bergqvi.st>
2022-07-13 09:04:19 +0200

    servo

--- servotest2.py
+++ servotest2.py
@@ -12,15 +12,6 @@ PULSE_FREQ = 50
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(OUT_PIN, GPIO.OUT)
 
-# def SetAngle(angle):
-#     duty = angle / 18 + 2
-#     GPIO.output(03, True)
-#     pwm.ChangeDutyCycle(duty)
-#     sleep(1)
-#     GPIO.output(03, False)
-#     pwm.ChangeDutyCycle(0)
-
-
 def main():
     print("Starting")
     servo1 = GPIO.PWM(OUT_PIN, PULSE_FREQ)
@@ -29,13 +20,10 @@ def main():
 
     print("Spinning")
 
-    # Test the full range of movement. Note only integers are allowed.
-    # for x in range(2, 12):
-    #     servo1.ChangeDutyCycle(x)
-    #     time.sleep(0.5)
-
     angle = 90
     duty = angle / 18 + 2
+
+    print(f'{angle}, {duty}')
 
     GPIO.output(OUT_PIN, True)
     servo1.ChangeDutyCycle(duty)
@@ -43,38 +31,7 @@ def main():
     GPIO.output(OUT_PIN, False)
     servo1.ChangeDutyCycle(0)
 
-    print(f'{angle}, {duty}')
 
-    # Start over and move in bigger, slower movements.
-    # servo1.ChangeDutyCycle(7)
-    # time.sleep(10)
-    return
-
-    time.sleep(1)
-    servo1.ChangeDutyCycle(7)
-    time.sleep(1)
-    servo1.ChangeDutyCycle(12)
-    time.sleep(4)
-
-    # Jump between the opposite positions.
-    servo1.ChangeDutyCycle(2)
-    time.sleep(1)
-    servo1.ChangeDutyCycle(12)
-    time.sleep(1)
-    servo1.ChangeDutyCycle(2)
-    time.sleep(1)
-    servo1.ChangeDutyCycle(12)
-    time.sleep(4)
-
-    # Test the fastest movement possible - no sleeping.
-    servo1.ChangeDutyCycle(2)
-    servo1.ChangeDutyCycle(12)
-    servo1.ChangeDutyCycle(2)
-    servo1.ChangeDutyCycle(12)
-    servo1.ChangeDutyCycle(2)
-    servo1.ChangeDutyCycle(12)
-    servo1.ChangeDutyCycle(2)
-    servo1.ChangeDutyCycle(12)
 
     servo1.stop()
     GPIO.cleanup()
