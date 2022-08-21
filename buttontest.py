52fb19f6be3d3073222a3f89fb0f9ff5c5d24f8e
Peppe Hämeenniemi <p@bergqvi.st>
2022-08-21 17:35:19 +0200

    button test

--- buttontest.py
+++ buttontest.py
@@ -1,13 +1,11 @@
 import RPi.GPIO as GPIO
 
-
 GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setmode(GPIO.BCM) # Use physical pin numbering
 
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.setup(32, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-if GPIO.input(32):
-    print('Input was HIGH')
+if GPIO.input(12):
+     print('Input was HIGH')
 else:
-    print('Input was LOW')
+     print('Input was LOW')
