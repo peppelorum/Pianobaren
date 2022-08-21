90518dc4b2e0042a0714d35bc7a324989889bdeb
Peppe Hämeenniemi <p@bergqvi.st>
2022-08-21 17:34:34 +0200

    button test

--- buttons.py
+++ buttons.py
@@ -50,17 +50,22 @@ def stop(channel):
     f()
 
 GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setmode(GPIO.BCM) # Use physical pin numbering
 
 
-GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(7,GPIO.RISING,callback=unload_cassette, bouncetime=1500)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(4,GPIO.RISING,callback=unload_cassette, bouncetime=1500)
 
-GPIO.setup(29, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(29,GPIO.RISING,callback=pitch, bouncetime=1500)
+GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(5,GPIO.RISING,callback=pitch, bouncetime=1500)
 
-GPIO.setup(31, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(31,GPIO.RISING,callback=play, bouncetime=1500)
+# PLAY
+GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(6,GPIO.RISING,callback=play, bouncetime=1500)
+
+# STOP
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(12,GPIO.RISING,callback=play, bouncetime=1500)
 
 GPIO.setup(13, GPIO.OUT)
 pwm=GPIO.PWM(13, 50)
--- buttontest.py
+++ buttontest.py
@@ -1,6 +1,9 @@
 import RPi.GPIO as GPIO
 
 
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+
 
 GPIO.setup(32, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
