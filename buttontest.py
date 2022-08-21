7f1d19a5aa777a832b47b23794def30b7342c307
Peppe Hämeenniemi <p@bergqvi.st>
2022-08-21 20:40:49 +0200

    button test

--- buttontest.py
+++ buttontest.py
@@ -1,11 +1,30 @@
-import RPi.GPIO as GPIO
 
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BCM) # Use physical pin numbering
 
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-if GPIO.input(12):
-     print('Input was HIGH')
-else:
-     print('Input was LOW')
+from signal import pause
+from time import sleep
+from gpiozero import Button
+
+
+button = Button(13)
+
+
+def unload():
+    print('unload')
+
+button.when_pressed = unload
+pause()
+
+
+
+# import RPi.GPIO as GPIO
+
+# GPIO.setwarnings(False) # Ignore warning for now
+# GPIO.setmode(GPIO.BCM) # Use physical pin numbering
+
+# GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# if GPIO.input(12):
+#      print('Input was HIGH')
+# else:
+#      print('Input was LOW')
