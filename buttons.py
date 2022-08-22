8bc543cc2ecc51fc37bb52fd923a3fffe5504b01
Peppe Hämeenniemi <p@bergqvi.st>
2022-08-22 11:08:15 +0200

    button test

--- buttons.py
+++ buttons.py
@@ -21,7 +21,7 @@ from debounce import debounce
 # 17 = 11, servo
 
 
-servo = AngularServo(17, min_angle=-90, max_angle=90)
+servo = AngularServo(17, min_angle=-90, max_angle=180)
 
 unload_button = Button(4)
 pitch_button = Button(27)
@@ -30,60 +30,35 @@ annan_button = Button(22, bounce_time=0.1)
 play_button = Button(23, bounce_time=0.1)
 stop_button = Button(24, bounce_time=0.1)
 
-
-def setAngle():
-    servo.angle = -90
-    sleep(2)
-    servo.angle = -45
-    sleep(2)
-    servo.angle = 0
-    sleep(2)
-    servo.angle = 45
-    sleep(2)
-    servo.angle = 90
-    servo.detach()
-    sleep(2)
-
-    # pass
-    # servo = Servo(22)
-    # servo.value = 0.5
-    # duty = angle / 18 + 2
-    # GPIO.output(13, True)
-    # pwm.ChangeDutyCycle(duty)
-    # sleep(1)
-    # GPIO.output(13, False)
-    # pwm.ChangeDutyCycle(0)
-
 @debounce(0.2)
 def unload_cassette():
-    # conn = rpyc.connect("localhost", 12345)
     print("Unload was pushed!")
 
-    servo.angle = 90
+    servo.angle = 180
     sleep(1)
     servo.angle = 0
     servo.detach()
 
-    # setAngle(90)
-    # unload = rpyc.async_(conn.root.unload)
-    # unload()
+    conn = rpyc.connect("localhost", 12345)
+    unload = rpyc.async_(conn.root.unload)
+    unload()
 
 @debounce(0.2)
 def pitch():
-    # conn = rpyc.connect("localhost", 12345)
     print("Pitch was pushed!")
-    # f = rpyc.async_(conn.root.pitch)
-    # f()
+    conn = rpyc.connect("localhost", 12345)
+    f = rpyc.async_(conn.root.pitch)
+    f()
 
-def play(channel):
-    # conn = rpyc.connect("localhost", 12345)
+def play():
     print("play was pushed!")
-    # f = rpyc.async_(conn.root.play)
-    # f()
+    conn = rpyc.connect("localhost", 12345)
+    f = rpyc.async_(conn.root.play)
+    f()
 
-def stop(channel):
-    # conn = rpyc.connect("localhost", 12345)
+def stop():
     print("stop was activated!")
+    # conn = rpyc.connect("localhost", 12345)
     # f = rpyc.async_(conn.root.pitch)
     # f()
 
@@ -95,9 +70,6 @@ play_button.when_pressed = play
 stop_button.when_released = stop
 
 
-
-
-# setAngle(90)
 
 servo.angle = 0
 servo.detach()
